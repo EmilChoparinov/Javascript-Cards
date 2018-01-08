639049a8f8795a0b5047cdfe8d0a7712e1ebf4ab
EmilChoparinov <emilchoparinov@gmail.com>
2018-01-08 12:50:18 -0800

    finished basic deck class

--- Deck.js
+++ Deck.js
@@ -1,16 +1,48 @@
 var reference = require('./Card');
-var card = reference.Card;
-class Deck{
-    constructor(){
+var Card = reference.Card;
+
+class Deck {
+    constructor() {
         this.deck = [];
+        this.reset();
     }
-    reset(){
 
+    reset() {
+        for (let suit = 0; suit < 4; suit++) {
+            for (let value = 1; value <= 13; value++) {
+                this.deck.push(new Card(value, suit));
+            }
+        }
+        return this;
     }
-    deal(){
 
+    shuffle() {
+        let swap = function (arr, i, j) {
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        };
+        for (let i = 0; i < this.deck.length; i++) {
+            let nextSwap = Math.floor(Math.random() * this.deck.length);
+            swap(this.deck, i, nextSwap);
+        }
+        return this;
     }
-    toString(){
-        
+
+    deal() {
+        return this.deck.pop();
+    }
+
+    toString() {
+        let s = 'The current deck:\n';
+        for (let i = 0; i < this.deck.length; i++) {
+            s += this.deck[i].toString() + '\n';
+        }
+        s += `${this.deck.length} cards total`;
+        return s;
     }
 }
+
+let deck = new Deck();
+deck.shuffle().shuffle();
+console.log(deck.toString());
